47b6f6c4f5e15b2af9521eaade4af652a80648e7
rajutheprogrammer <onlyrchandra@gmail.com>
2021-05-21 13:45:46 +0530

    first commit

---
+++ assets/main.js
@@ -0,0 +1,31 @@
+var canvas=document.getElementById("mycanvas");
+var ctx=canvas.getContext("2d");
+
+
+/*ctx.moveTo(300,0);
+ctx.lineTo(50,50);
+ctx.stroke();
+
+ctx.moveTo(300,150);
+ctx.lineTo(50,50);
+ctx.stroke();
+ctx.moveTo(0,0);
+ctx.lineTo(50,50);
+ctx.stroke();
+
+ctx.moveTo(0,150);
+ctx.lineTo(50,50);
+ctx.stroke();*/
+
+ctx.beginPath();
+    ctx.moveTo(175, 50);
+    ctx.lineTo(100, 75);
+    ctx.lineTo(100, 25);
+    ctx.lineTo(175, 50);
+    ctx.stroke();
+    
+
+
+
+
+
